f17fd8ac5e22cbbde2e3eac94d622801b59faeec
kay@kay-no-linux on Jetson <nitt0x0@gmail.com>
2020-10-09 18:14:45 +0900

    1.8-modif canny detect

--- lecture_02/video_in.py
+++ lecture_02/video_in.py
@@ -5,19 +5,24 @@
 import sys
 import cv2
 
-from _path import (DIR_HOME, get_cut_dir, stop_if_none)
+from _path import (DIR_SRC, get_cut_dir, stop_if_none)
 
-dir_avi = DIR_HOME + 'src\\avi\\'
+dir_avi = DIR_SRC + 'avi/'
+# video_name = '201907-01.mp4'    # stir-separate
+# video_name = '201907-02.mp4'
+video_name = '201907-03.mp4'
+# video_name = '202006-01.mp4'
 
 
 # 동영상 파일로부터 cv2.VideoCapture 객체 생성
 # if not cap.isOpened():
 # cap = cv2.VideoCapture(0)     #Device #0 = CAMERA_ON
-cap = cv2.VideoCapture(dir_avi + 'input.avi')
-cap = stop_if_none(cap, message="Camera open failed!")
+
+cap = cv2.VideoCapture(dir_avi + video_name)
+# cap = stop_if_none(cap, message="Camera open failed!")
 
 # 프레임 크기
-width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH) 
 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
 count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
@@ -33,14 +38,26 @@ print('FPS:', fps)
 delay = round(1000 / fps)
 
 # 매 프레임 처리 및 화면 출력
+winName0 = 'Original Image'
+winName1 = 'Canny Edge Detection'
+
+sizeRate = 0.43
+winResize = (int(width * sizeRate) , int(height * sizeRate))
+moveTo = (0, winResize[1]+70)
+
 while True:
     _, frame = cap.read()
     frame = stop_if_none(frame, message="No Video Input!")
+    frame = cv2.resize(frame, winResize)   
 
     edge = cv2.Canny(frame, 50, 150)
+    edge = cv2.resize(edge, winResize)   
 
-    cv2.imshow('frame', frame)
-    cv2.imshow('edge', edge)
+    cv2.imshow(winName0, frame)
+    cv2.moveWindow(winName0, 0,0)
+
+    cv2.imshow(winName1, edge)
+    cv2.moveWindow(winName1, *moveTo)
 
     if cv2.waitKey(delay) == 27:
         break
